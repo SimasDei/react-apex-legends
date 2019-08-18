753dfc053569b6976753d36ceb0578383bfacdf8
SimasDei <simondeiman@gmail.com>
2019-08-18 14:31:44 +0300

    add getProfile route logic

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const router = express.Router();
+const fetch = require('node-fetch');
 
-router.get('/:platform/:gamertag', (req, res) => {
-  console.log(req.params.platform, req.params.gamertag);
+router.get('/:platform/:gamertag', async (req, res) => {
+  try {
+    const headers = {
+      'TRN-Api-Key': process.env.TRACKER_API_KEY,
+    };
+
+    const { platform, gamertag } = req.params;
+
+    const response = await fetch(`${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`, {
+      headers,
+    });
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Server error' });
+  }
   res.send('Ahoy Sailor o/');
 });
 
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 5500;
 
 app.use('/api/v1/profile', require('./routes/profile'));
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.listen(port, () => {
   console.log(`Backend server running on port: ${port}, Mode: ${process.env.NODE_ENV}`);
 });
