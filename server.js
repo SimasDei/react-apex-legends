f495e949638f4d7c0685aaf06890b32d8575bef9
SimasDei <simondeiman@gmail.com>
2019-08-18 14:17:10 +0300

    add profile router

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ dotenv.config({ path: './config.env' });
 const app = express();
 const port = process.env.PORT || 5500;
 
-app.get('/api/v1/profile/:platform/:gamertag', (req, res) => {
-  console.log(req.params.platform, req.params.gamertag);
-  res.send('Ahoy Sailor o/');
-});
+app.use('/api/v1/profile', require('./routes/profile'));
 
 app.listen(port, () => {
   console.log(`Backend server running on port: ${port}, Mode: ${process.env.NODE_ENV}`);
