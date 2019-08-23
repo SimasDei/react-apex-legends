6d52d3b784cc2ab1958eb29daedb1e6065bd02fe
SimasDei <simondeiman@gmail.com>
2019-08-23 21:09:26 +0300

    add Header and Search components

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,12 +1,28 @@
 <template>
-  <section class="header"></section>
+  <header>
+    <img :src="image" />
+  </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
+  data() {
+    return {
+      image: require('@/assets/logo.png'),
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped>
+img {
+  width: 300px;
+}
+@media (max-width: 700px) {
+  img {
+    display: block;
+    margin: auto;
+  }
+}
 </style>
